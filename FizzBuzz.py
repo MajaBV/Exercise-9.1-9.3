a8974327128b3b622b58396dfa3e8176995860f6
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-11 23:12:19 +0100

    fixed fizzbuzz, fizz and buzz order and adding stop number

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,12 @@
-guess = input("Select a number between 1 and 100: ")
+select_number = int(input("Select a number between 1 and 100: "))
 
-for i in range(1, 101):
-    if i % 3 == 0:
+for i in range(1, select_number+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
         print("fizz")
     elif i % 5 == 0:
         print("buzz")
-    elif i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
     else:
         print(i)
 
