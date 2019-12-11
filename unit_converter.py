1d7d2b502894cf183635c29ae10912b6ba6d9469
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-11 20:31:30 +0100

    moving float and adding replace , with . in line 6

--- unit_converter.py
+++ unit_converter.py
@@ -2,9 +2,10 @@ print("Hello there. This is kilometers into miles converter.")
 
 while True:
     print("Please enter below the number of kilometers that you'd like to convert into miles.")
-    km = float(input("Kilometers: "))
-
+    km = (input("Kilometers: "))
+    km = float(km.replace(",", "."))
     miles = km * 0.621371192
+
     print("{} km = {} miles".format(km, miles))
 
     search = input("Do you want another conversion (y/n): ")
