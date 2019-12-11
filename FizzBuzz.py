258342a05d66fb2a5c08b0cea78890845f42535a
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-11 22:02:08 +0100

    adding buzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,8 +3,12 @@ guess = input("Select a number between 1 and 100: ")
 for i in range(1, 101):
     if i % 3 == 0:
         print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
     else:
         print(i)
+
+
 
 
 
