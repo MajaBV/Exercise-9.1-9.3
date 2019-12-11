71f2d5576d7ead2423edb056465fbb7ab70628b7
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-11 22:05:23 +0100

    adding fizzbuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,6 +5,8 @@ for i in range(1, 101):
         print("fizz")
     elif i % 5 == 0:
         print("buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
     else:
         print(i)
 
