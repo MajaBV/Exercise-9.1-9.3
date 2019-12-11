865af2029b7a0cc646e9e242ec71de0c48f22e04
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-11 18:50:49 +0100

    initial commit

---
+++ unit_converter.py
@@ -0,0 +1,12 @@
+print("Hello there. This is kilometers into miles converter.")
+
+while True:
+    print("Please enter below the number of kilometers that you'd like to convert into miles.")
+    km = float(input("Kilometers: "))
+
+    miles = km * 0.621371192
+    print(miles)
+
+    search = input("Do you want another conversion (y/n): ")
+    if search.lower() != "yes" and search.lower() != "y":
+        break
